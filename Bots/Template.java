c13534356c8b7cdc8f52ac4a7ddd1892e007fee1
Choco31415 <Eparke31415@gmail.com>
2015-01-09 17:53:35 -0500

    Create Template.java

---
+++ Bots/Template.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Template {
+	Position position;
+	String name;
+	ArrayList<Link> links = null;
+	
+	public Template(Position pos_, String name_, ArrayList<Link> links_) {
+		position = pos_;
+		name = name_;
+		links = links_;
+	}
+	
+	public Template(Position pos_, String name_) {
+		position = pos_;
+		name = name_;
+		links = null;
+	}
+	
+	public void setLinks(ArrayList<Link> links_) {
+		links = links_;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public ArrayList<Link> getLinks() {
+		return links;
+	}
+	
+	public boolean containsLink(String link) {
+		for (int i = 0; i < links.size(); i++) {
+			if ((links.get(i)).getDestination() == link) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
