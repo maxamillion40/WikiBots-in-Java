709b6f42659d2d22e267a02fe140f7a333afb586
Choco31415 <Eparke31415@gmail.com>
2015-01-14 15:16:36 -0500

    Update Template.java
    Templates now store info on images in parameters. Possible other tweaks included. Do not know.

--- Bots/Template.java
+++ Bots/Template.java
@@ -3,18 +3,29 @@ import java.util.ArrayList;
 public class Template {
 	Position position;
 	String name;
-	ArrayList<Link> links = null;
+	ArrayList<String> parameters = new ArrayList<String>();
+	ArrayList<Image> images = new ArrayList<Image>();
+	ArrayList<Link> links = new ArrayList<Link>();
 	
-	public Template(Position pos_, String name_, ArrayList<Link> links_) {
+	public Template(Position pos_, String name_, ArrayList<String> params, ArrayList<Image> images_, ArrayList<Link> links_) {
 		position = pos_;
 		name = name_;
+		parameters = params;
+		images = images_;
 		links = links_;
 	}
 	
 	public Template(Position pos_, String name_) {
 		position = pos_;
 		name = name_;
-		links = null;
+	}
+	
+	public void addLink(Link link) {
+		links.add(link);
+	}
+	
+	public void addImage(Image image) {
+		images.add(image);
 	}
 	
 	public void setLinks(ArrayList<Link> links_) {
@@ -29,13 +40,33 @@ public class Template {
 		return name;
 	}
 	
+	public int getParameterCount() {
+		return parameters.size();
+	}
+	
+	public ArrayList<String> getParameters() {
+		return parameters;
+	}
+	
+	public String getParameter(int i) {
+		return parameters.get(i);
+	}
+	
+	public ArrayList<Image> getImages() {
+		return images;
+	}
+	
+	public Image getImage(int i) {
+		return images.get(i);
+	}
+	
 	public ArrayList<Link> getLinks() {
 		return links;
 	}
 	
-	public boolean containsLink(String link) {
+	public boolean containsLink(Link link) {
 		for (int i = 0; i < links.size(); i++) {
-			if ((links.get(i)).getDestination() == link) {
+			if ((links.get(i)).equals(link)) {
 				return true;
 			}
 		}
@@ -50,6 +81,7 @@ public class Template {
 		for (int i = 0; i < links.size(); i++) {
 			output += (links.get(i) + " , ");
 		}
+		output += "\nAt: " + position;
 		return output;
 	}
 }
