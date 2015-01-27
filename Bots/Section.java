9e088c2d0310504b0f1f4d7945f71c213ecda485
Choco31415 <Eparke31415@gmail.com>
2015-01-27 09:19:18 -0500

    Create Section.java
    Created a new data type.

---
+++ Bots/Section.java
@@ -0,0 +1,28 @@
+public class Section {
+	Position position;
+	String title;
+	int depth;
+	
+	public Section(Position pos_, String title_, int depth_) {
+		position = pos_;
+		title = title_;
+		depth = depth_;
+	}
+	
+	public Position getPosition() {
+		return position;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public int getDepth() {
+		return depth;
+	}
+	
+	@Override
+	public String toString() {
+		return "(Section) Title: " + title + " Depth: " + depth + " (Position: " + position + ")";
+	}
+}
