3423ca4c7d5e9e19c568bdf83fe2ee02130a6cc1
Choco31415 <Eparke31415@gmail.com>
2015-01-29 15:17:56 -0500

    Update Revision.java
    Fleshed out.

--- Bots/Revision.java
+++ Bots/Revision.java
@@ -1,3 +1,33 @@
-public class Revision {
+import java.sql.Date;
 
+public class Revision {
+	String user;
+	String comment;
+	Date date;
+	Page page = null;
+	
+	public Revision(String user_, String comment_, Date date_, Page page_) {
+		user = user_;
+		comment = comment_;
+		date = date_;
+		page = page_;
+	}
+	
+	public Revision(String user_, String comment_, Date date_) {
+		user = user_;
+		comment = comment_;
+		date = date_;
+	}
+	
+	public String getUser() {
+		return user;
+	}
+	
+	public String getComment() {
+		return comment;
+	}
+	
+	public Date getDate() {
+		return date;
+	}
 }
